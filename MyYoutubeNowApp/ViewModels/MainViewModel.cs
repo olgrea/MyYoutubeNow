adeb324796cd27bde5cee85fc2f9b64cb1d41af7
olgrea <olgrea@gmail.com>
2023-11-01 13:10:38 -0400

    retrieve url info WIP

--- MyYoutubeNowApp/ViewModels/MainViewModel.cs
+++ MyYoutubeNowApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,42 @@
-﻿namespace MyYoutubeNowApp.ViewModels;
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyYoutubeNow;
+using YoutubeExplode.Playlists;
+using YoutubeExplode.Videos;
 
-public partial class MainViewModel : ViewModelBase
+namespace MyYoutubeNowApp.ViewModels;
+
+public partial class MainViewModel : ObservableValidator
 {
-    public string Greeting => "Welcome to Avalonia!";
+    // TODO : dependency injection
+    private MyYoutubeNowService _myn;
+
+    public MainViewModel(MyYoutubeNowService myn)
+    {
+        _myn = myn;
+    }
+
+    public string Url { get; set; } = string.Empty;
+    
+    public VideoListViewModel? Video { get; set; }
+
+    [RelayCommand]
+    private async Task GetUrlInfo()
+    {
+        if(MyYoutubeNowService.IsVideo(Url))
+        {
+            Video vid = await _myn.GetVideoInfoAsync(Url);
+        }
+        else if(MyYoutubeNowService.IsPlaylist(Url)) 
+        {
+            Playlist pl = await _myn.GetPlaylistInfoAsync(Url);
+        }
+        else
+        {
+            throw new ArgumentException("Unrecognized url");
+        }
+    }
+
 }
--- MyYoutubeNowApp/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace MyYoutubeNowApp.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
