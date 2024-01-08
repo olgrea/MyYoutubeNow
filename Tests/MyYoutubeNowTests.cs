ce85e92664a9672bebb9edd5e8330d8ac2b58fb4
olgrea <olgrea@gmail.com>
2024-01-08 09:07:10 -0500

    GetVideoInfoAsync tests

--- Tests/MyYoutubeNowTests.cs
+++ Tests/MyYoutubeNowTests.cs
@@ -1,12 +1,39 @@
 using NUnit.Framework;
+using MyYoutubeNow;
+using System.Threading.Tasks;
+using System;
+using YoutubeExplode.Videos;
 
 namespace Tests
 {
     public class MyYoutubeNowTests
     {
-        [Test]
-        public void TestMethod1()
+        MyYoutubeNowService _myns;
+
+        [SetUp] 
+        public void SetUp() 
         {
+            _myns = new MyYoutubeNowService();
+        }
+
+        [Test]
+        public async Task GetVideoInfoAsync_ValidUrl_RetrievesIt()
+        {
+            string id = "atBi_MfT3LE";
+            string url = $"https://www.youtube.com/watch?v={id}";
+            Video vid = await _myns.GetVideoInfoAsync(url);
+
+            Assert.That(vid, Is.Not.Null);
+            Assert.That(vid.Id.ToString(), Is.EqualTo(id));
+        }
+
+        [Test]
+        public void GetVideoInfoAsync_InvalidUrl_Throws()
+        {
+            string invalidUrl1 = "https://www.youtube.com/watch?v=ssssssssss";
+            string invalidUrl2 = "dqasdfawq";
+            Assert.ThrowsAsync<ArgumentException>(() =>  _myns.GetVideoInfoAsync(invalidUrl1));
+            Assert.ThrowsAsync<ArgumentException>(() =>  _myns.GetVideoInfoAsync(invalidUrl2));
         }
     }
 }
